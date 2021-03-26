0e04228deb776a3390aa002b1e7ba93096825afe
Seyhan Akifov <55700582+Kabalsky@users.noreply.github.com>
2021-03-26 16:11:03 +0100

    Added transient services on web.core

--- Quiz.Web/Startup.cs
+++ Quiz.Web/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Quiz.Data;
+using Quiz.Services;
 
 namespace Quiz.Web
 {
@@ -29,6 +30,11 @@ namespace Quiz.Web
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+
+            services.AddTransient<IQuizService, QuizService>();
+            services.AddTransient<IQuestionService, QuestionService>();
+            services.AddTransient<IAnswerService, AnswerService>();
+            services.AddTransient<IUserQuizService, UserQuizService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
